6f62831c0caee43dcb7b5f88267ade3f8b257674
Mindaugas Karpinskas <mkarpinskas@exadel.com>
2017-10-12 15:30:42 +0300

    New project added

---
+++ AntraSavaite/src/lt/codeacademy/studies/javabasica/secondweek/operators/Uzduotis1.java
@@ -0,0 +1,21 @@
+package lt.codeacademy.studies.javabasica.secondweek.operators;
+
+public class Uzduotis1 {
+
+	public static void main(String[] args) {
+		String sakinys = ivesti();
+		int indeksas1 = 0;
+		int indeksas2 = sakinys.indexOf(' ');
+
+		String z1 = sakinys.substring(indeksas1, indeksas2);
+		
+		
+		
+	}
+
+	private static String ivesti() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
