7d2736e5632758612e032ba64565cf7d823e842a
Mindaugas Karpinskas <mkarpinskas@exadel.com>
2018-09-27 10:39:16 +0300

    pridėta užduotis

--- AntraSavaite/src/lt/codeacademy/studies/javabasica/secondweek/operators/Uzduotis1.java
+++ AntraSavaite/src/lt/codeacademy/studies/javabasica/secondweek/operators/Uzduotis1.java
@@ -5,12 +5,11 @@ public class Uzduotis1 {
 	public static void main(String[] args) {
 		String sakinys = ivesti();
 		int indeksas1 = 0;
-		int indeksas2 = sakinys.indexOf(' ');
+		int indeksas2 = sakinys.length(); // pakeisti
 
 		String z1 = sakinys.substring(indeksas1, indeksas2);
-		
-		
-		
+
+		System.out.println("Pirmas sakinio žodis: " + z1);
 	}
 
 	private static String ivesti() {
